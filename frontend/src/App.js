d463d9c73e6f6880a065184ead008f65b3cdf867
LerichO <lerichyaso@gmail.com>
2024-01-27 14:01:49 -0600

    started stucture of react app and gmail login handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import GmailLogin from './components/gmail-login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleOAuthProvider clientId="217156761187-nd7snmr4dgfbgrailbchl9357st6drme.apps.googleusercontent.com">
+      </GoogleOAuthProvider>
     </div>
   );
 }
