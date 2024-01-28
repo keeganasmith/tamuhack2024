bef6c3452dc9d568d5007cace5936c442d9ceac3
Michael Morris <MRMorris64@tamu.edu>
2024-01-28 09:46:31 -0600

    "Slightly better login"

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -8,14 +8,16 @@ const LoginScreen = ({ setEmails }) => {
     console.log("client: " + process.env.REACT_APP_GOOGLE_CLIENT_ID);
     return (
         <div className="login-container">
-          <div className = "text-container">
-            <h1>Welcome!</h1>
-            <h1>Sign in to get started!</h1>
-          </div>
-          <div className = "gmail-container">
-            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-              <GmailLogin navigation={history} setEmails={setEmails}/>
-            </GoogleOAuthProvider>
+          <div className = "info-container">
+            <div className = "text-container">
+              <h1>Welcome!</h1>
+              <h1>Sign in to get started!</h1>
+            </div>
+            <div className = "gmail-container">
+              <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+                <GmailLogin navigation={history} setEmails={setEmails}/>
+              </GoogleOAuthProvider>
+            </div>
           </div>
         </div>
     );
