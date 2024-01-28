7043f471785adf1dfedaee2b19392f7cc2c66321
keeganasmith2003 <99233684+keeganasmith@users.noreply.github.com>
2024-01-28 00:33:41 -0600

    enable cors

---
+++ backend/tests/http/test_deploy.sh
@@ -0,0 +1,3 @@
+curl -X POST https://phishnetasdf.onrender.com/api/get_emails\
+     -H "Content-Type: application/json" \
+     -d '{"token": "ya29.a0AfB_byDSWjboXFJfiNXdmFLCeD_clw3lokjin6musQ8Kmb-rdz_VUw3Zvgck551GoBFt1-RyG1hNWIbeVunr_iYWUnHQ1C8pPN2_u3GaGy31ewAAa1jVvYTwQBdGdGiegdz0jJ7xgfJ9nj4vTbscAi_0zfwp_ti3RzhXaCgYKAa8SARESFQHGX2MiDSJyO6uQx1V1Yssb7a-L9Q0171"}'
