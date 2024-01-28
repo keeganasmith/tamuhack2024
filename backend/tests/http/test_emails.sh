946c1ef37928f99100ae980d919374d87a27062e
Keegan Smith <keeganasmith2003@tamu.edu>
2024-01-27 19:52:43 -0600

    endpoints for stuff

---
+++ backend/tests/http/test_emails.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:5000/api/get_emails\
+     -H "Content-Type: application/json" \
+     -d '{"key1": "value1", "key2": "value2"}'
---
+++ backend/tests/http/test_post_req.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:5000/api/get_emails\
+     -H "Content-Type: application/json" \
+     -d '{"access_token": "ya29.a0AfB_byAvw4_zX2BdzuFyWh5hnscMQRSVldjmy13U4AS1-Ueh5ZsPYBwG3g6LL1WliIEwnXjcb8Y0AQwhmonIyBXB_MqbRDCEpERnZN57ptmOkO4XQwzapUVS2BfKYJohcz_pVh8QMuzu2-anRRsr9nR47wTCUjMjpLUFaCgYKARUSARASFQHGX2Mi1kTk52a9tQtfbG6pOxcz_A0171"}'
