310ba2804c262ef3d9c204ed0f94b1d0b8e8611a
LerichO <lerichyaso@gmail.com>
2024-01-28 09:09:20 -0600

    merging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EmailScreen from './screens/EmailScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -6,7 +6,7 @@ import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
 
-  
+
   const initialEmails = [
     { id: 1, sender: 'person@yahoo', subject: 'subject123', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Tincidunt eget nullam non nisi est. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Consequat nisl vel pretium lectus. Dolor sit amet consectetur adipiscing elit ut aliquam. Diam ut venenatis tellus in metus. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Nisl vel pretium lectus quam id leo in vitae. Est sit amet facilisis magna etiam tempor orci. Id venenatis a condimentum vitae sapien. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Mauris nunc congue nisi vitae. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Dolor magna eget est lorem. Sit amet aliquam id diam maecenas.' },
     { id: 2, sender: 'bob@gmail', subject: 'subject245', content: 'Lorem ipsum dolor elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Tincidunt eget nullam non nisi est. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Consequat nisl vel pretium lectus. Dolor sit amet consectetur adipiscing elit ut aliquam. Diam ut venenatis tellus in metus. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Nisl vel pretium lectus quam id leo in vitae. Est sit amet facilisis magna etiam tempor orci. Id venenatis a condimentum vitae sapien. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Mauris nunc congue nisi vitae. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Dolor magna eget est lorem. Sit amet aliquam id diam maecenas.' },
@@ -16,12 +16,19 @@ const App = () => {
   const [emails, setEmails] = useState(initialEmails);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginScreen setEmails={setEmails} />} />
-        <Route path="/email" element={<EmailScreen emails={emails}/>} />
-      </Routes>
-    </Router>
+    <div>
+      <header className='App-header'>
+        <img src="" />
+        <h1>Phish Net</h1>
+      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginScreen setEmails={setEmails} />} />
+          <Route path="/email" element={<EmailScreen emails={emails} />} />
+        </Routes>
+      </Router>
+    </div>
+
   );
 };
 
--- frontend/src/components/gmail-login.js
+++ frontend/src/components/gmail-login.js
@@ -9,12 +9,6 @@ function GmailLogin({ setEmails }) {
         onSuccess: async (code) => {
             console.log("Login: ", code);
       
-            const temp_email = [
-              { id: 1, sender: 'person@yahoo', subject: 'subject123', content: 'Lorem ipsum dolor sit amet, consecteTincidunt eget nullam non nisi est. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Consequat nisl vel pretium lectus. Dolor sit amet consectetur adipiscing elit ut aliquam. Diam ut venenatis tellus in metus. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Nisl vel pretium lectus quam id leo in vitae. Est sit amet facilisis magna etiam tempor orci. Id venenatis a condimentum vitae sapien. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Mauris nunc congue nisi vitae. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Dolor magna eget est lorem. Sit amet aliquam id diam maecenas.' },
-              { id: 2, sender: 'bob@gmail', subject: 'subject245', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Tincidunt eget nullam non nisi est. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Consequat nisl vel pretium lectus. Dolor sit amet consectetur adipiscing elit ut aliquam. Diam ut venenatis tellus in metus. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Nisl vel pretium lectus quam id leo in vitae. Est sit amet facilisis magna etiam tempor orci. Id venenatis a condimentum vitae sapien. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Mauris nunc congue nisi vitae. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Dolor magna eget est lorem. Sit amet aliquam id diam maecenas.' },
-              // Add more emails as needed
-            ];
-      
             try {
               // Make an asynchronous API call using Axios
               const response = await axios.post("https://phishnetasdf.onrender.com/api/get_emails", { "token": code.access_token });
